271619512a85c992f0794fad87962dfe68fbae4e
Rodrigo Bouzas <r.bouzas@liceolapaz.net>
2025-03-14 00:05:36 +0100

    Restablece funcionalidad de búsqueda y paginación.

--- frontend/src/client/useListaProductos.js
+++ frontend/src/client/useListaProductos.js
@@ -1,17 +1,24 @@
-import { useMemo } from "react";
+import { useEffect } from "react";
 import useFetch from "./useFetch";
 
 const useListaProductos = (offset, limit, search) => {
-  const params = useMemo(() => {
-    const result = new URLSearchParams();
-    const searchEnabled = search && search.length > 3;
-    if (searchEnabled) result.set("search", search);
-    result.set("offset", offset);
-    if (limit) result.set("limit", limit);
-    return result;
-  }, [offset, limit, search]);
+  const params = new URLSearchParams();
 
-  return useFetch({ url: `/api/productos?${params}` }, true);
+  const searchEnabled = search && search.length > 3;
+  if (searchEnabled) params.set("search", search);
+  params.set("offset", offset);
+  if (limit) params.set("limit", limit);
+  const paramsString = params.toString();
+
+  const fetchHook = useFetch({ url: `/api/productos?${params}` }, false);
+  const { request } = fetchHook;
+
+  useEffect(() => {
+    request && request();
+    return () => {};
+  }, [request, paramsString]);
+
+  return fetchHook;
 };
 
 export default useListaProductos;
